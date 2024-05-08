612f6e639033e66e49b24f2b4c1fc8aa9b2f2b41
psiconervio <psiconervio@gmail.com>
2024-05-08 18:16:23 -0300

    asd

--- src/index.js
+++ src/index.js
@@ -9,23 +9,23 @@ import { connectDB } from './db.js';
 
 connectDB();
 //prueba
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
 //app.listen(port);
 //console.log(port)
 
-app.get("/",(req,res)=>{
-    const htmlresponse= `<html>
-    <head>
-        <title>
-            Hello World
-        </title>
-    </html>`;
-    res.send(htmlresponse);
-});
+//app.get("/",(req,res)=>{
+//    const htmlresponse= `<html>
+//    <head>
+//        <title>
+//            Hello World
+//        </title>
+//    </html>`;
+//    res.send(htmlresponse);
+//});
 //
 //app.listen(port, () => {
 //  console.log(`Server is running on port uno y ${port}`);
 //});
 //oficial
-//app.listen(3000);
-//console.log('listening on port 3000',3000);
+app.listen();
+console.log('listening on port 3000',3000);
