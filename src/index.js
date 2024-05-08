b9b8658d526caf2cdf5c6fe61c774193d1114a00
psiconervio <psiconervio@gmail.com>
2024-05-08 18:32:49 -0300

    asas

--- src/index.js
+++ src/index.js
@@ -5,27 +5,28 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './db.js';
-
-
 connectDB();
 //prueba
 //const port = process.env.PORT || 3000;
 //app.listen(port);
 //console.log(port)
 
-//app.get("/",(req,res)=>{
-//    const htmlresponse= `<html>
-//    <head>
-//        <title>
-//            Hello World
-//        </title>
-//    </html>`;
-//    res.send(htmlresponse);
-//});
+app.get("/",(req,res)=>{
+    const htmlresponse= `<html>
+    <head>
+        <title>
+            Hello World
+        </title>
+        </head>
+    <body>
+        <h1>Hello World</h1>
+    </html>`;
+    res.send(htmlresponse);
+});
 //
 //app.listen(port, () => {
 //  console.log(`Server is running on port uno y ${port}`);
 //});
 //oficial
-app.listen();
-console.log('listening on port 3000',3000);
+//app.listen();
+//console.log('listening on port 3000',3000);
