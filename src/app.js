1052a0d65e93b92db789b6459adc3d80e8b2bdb1
psiconervio <psiconervio@gmail.com>
2024-05-08 18:59:40 -0300

    asas

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ dotenv.config();
 //}
 //app.use(cors(corsOptions));
 const app = express();
-app.use(cors(
+app.use(cors('*'
      //{
      //    origin:"https://mern-crud-auth-fatz.vercel.app/",
      //    credentials:true
