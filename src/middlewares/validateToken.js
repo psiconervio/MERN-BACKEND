677c786039855f777f1ee8c69cc43f9671ab0c31
psiconervio <psiconervio@gmail.com>
2024-05-13 10:00:18 -0300

    asd

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,5 +1,7 @@
 import  jwt  from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config.js";
+
+
 export const authRequired = (req,res,next) => {
     const {token} = req.cookies;
     if(!token) 
@@ -8,8 +10,9 @@ export const authRequired = (req,res,next) => {
     jwt.verify(token, TOKEN_SECRET, (err, user) => {
         if(err) return res.status(403).json({message: "token invalid"});
         req.user=user;
-console.log(user)
+        console.log(user);
+        next();
+
     });
-    next()
 
 }
