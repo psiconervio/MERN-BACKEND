b4753a97c6bd27fd36ef31ef4725a88fb4105b70
psiconervio <psiconervio@gmail.com>
2024-05-13 10:25:48 -0300

    assda

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -1,20 +1,56 @@
 import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config.js";
+import express from 'express';
+import cookieParser from 'cookie-parser';
 
-export function createAccessToken(payload) {
- return  new Promise((resolve, reject) => {
-        jwt.sign(
-            payload,
-            TOKEN_SECRET,
-        {
-            expiresIn: "1d", 
-        },
-        //callback
-        (err, token) => {
-            if(err) reject(err);
-            resolve(token);
+const app = express();
 
+// Asegúrate de usar el middleware de cookie-parser en tu aplicación
+app.use(cookieParser());
+
+export function createAccessToken(req, res, payload) {
+  return new Promise((resolve, reject) => {
+    jwt.sign(
+      payload,
+      TOKEN_SECRET,
+      {
+        expiresIn: "1d", 
+      },
+      //callback
+      (err, token) => {
+        if(err) reject(err);
+
+        // Establecer la cookie
+        res.cookie('token', token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'none'
         });
-    }
-    )};
+
+        resolve(token);
+      }
+    );
+  });
+};
+
+//codigo generico
+//import jwt from "jsonwebtoken";
+//import { TOKEN_SECRET } from "../config.js";
+//
+//export function createAccessToken(payload) {
+// return  new Promise((resolve, reject) => {
+//        jwt.sign(
+//            payload,
+//            TOKEN_SECRET,
+//        {
+//            expiresIn: "1d", 
+//        },
+//        //callback
+//        (err, token) => {
+//            if(err) reject(err);
+//            resolve(token);
+//
+//        });
+//    }
+//    )};
 //minuto 54 video fatz
--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -14,5 +14,5 @@ export const authRequired = (req,res,next) => {
         next();
 
     });
-
+//next() estaba aca
 }
